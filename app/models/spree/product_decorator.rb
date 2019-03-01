536ad93b00651d8be7daffa79c450e8858922e1c
eddxw <eduardo.jemb@gmail.com>
2019-02-28 20:43:35 -0700

    fixes

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Product.class_eval do
+  delegate :ean, :"#{:ean}=", to: :find_or_build_master
   whitelisted_ransackable_associations << 'product_properties'
   whitelisted_ransackable_associations << 'variant_properties'
 end
